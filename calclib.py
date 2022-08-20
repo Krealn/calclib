187c18aa98ce2ba3d89ca3538bee6d1bd8dc5c0d
BuildTools <unconfigured@null.spigotmc.org>
2022-08-20 18:53:47 +0400

    Created calclib for Python

---
+++ calclib.py
@@ -0,0 +1,8 @@
+def calc():
+    a = input("Enter first number: ")
+    b = input("Enter second number: ")
+    c = input("Enter function (+, -, /, *): ")
+    if c == "+":  print(a + b)
+    if c == "-":  print(a - b)
+    if c == "*":  print(a * b)
+    if c == "/":  print(a / b)
