ad2a52d84a2773daf9ef8fb240ad300a71ce80cc
Krealn <kapchenka2@mail.ru>
2022-08-20 19:33:00 +0400

    Modified readme.md (The last info about that), added returnCalc()

--- calclib.py
+++ calclib.py
@@ -10,3 +10,17 @@ def calc():
             print(int(a) / int(b))
         except:
             print("Dont try division to zero!")
+            
+def returnCalc():
+    a = input("Enter first number: ")
+    b = input("Enter second number: ")
+    c = input("Enter function (+, -, /, *): ")
+    if c == "+":  d = int(a) + int(b)
+    if c == "-":  d = int(a) - int(b)
+    if c == "*":  d = int(a) * int(b)
+    if c == "/":
+        try:
+            d = int(a) / int(b)
+        except:
+            d = "Dont try division to zero!"
+    return d
--- readme.md
+++ readme.md
@@ -1,1 +1,17 @@
+# Calclib for Python
+
 Calclib for Python - the best way to create fast calculator in your project!
+
+Just clone calclib.py to you project
+
+# How to use:
+
+import calclib
+
+calclib.calc()
+
+# or:
+
+import calclib
+
+print(calclib.returnCalc())
